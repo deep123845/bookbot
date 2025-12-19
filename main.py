ad77090ac9294e3ebd9a13828e1795938dc9ba4d
deep123845 <deep123845@gmail.com>
2025-12-18 22:56:24 -0500

    added code to read book text and count words

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+from stats import get_num_words
+
+def get_book_text(filepath):
+	with open(filepath) as f:
+		file_contents = f.read()
+	return file_contents
+
+def main():
+	book_text = get_book_text("books/frankenstein.txt")
+	num_words = get_num_words(book_text)
+	print(f"Found {num_words} total words")
+
+main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_num_words(input_string):
+	words = input_string.split()
+	return len(words)

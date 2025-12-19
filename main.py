aa4760630adf3836776e0a7ba78b43b84c1bf0a3
deep123845 <deep123845@gmail.com>
2025-12-18 23:02:54 -0500

    added function to count character frequencies

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_char_dict
 
 def get_book_text(filepath):
 	with open(filepath) as f:
@@ -9,5 +9,7 @@ def main():
 	book_text = get_book_text("books/frankenstein.txt")
 	num_words = get_num_words(book_text)
 	print(f"Found {num_words} total words")
+	char_dict = get_char_dict(book_text)
+	print(char_dict)
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,12 @@
 def get_num_words(input_string):
 	words = input_string.split()
 	return len(words)
+
+def get_char_dict(input_string):
+	char_dict = {}
+	for char in input_string:
+		lowercase_char = char.lower()
+		if not lowercase_char in char_dict:
+			char_dict[lowercase_char] = 0	
+		char_dict[lowercase_char] += 1
+	return char_dict	
